0b37a8bf25c3ac292bea465c2fcebb2081e5e975
Zefir <zefirkerozovv@gmail.com>
2026-07-10 12:51:32 +0300

    fix build

--- lib/media/folders.ts
+++ lib/media/folders.ts
@@ -1,3 +1,3 @@
-export type MediaFolder = "blog" | "events" | "products" | "popup" | "email";
+export type MediaFolder = "blog" | "events" | "products" | "guides" | "popup" | "email";
 
-export const MEDIA_FOLDERS: MediaFolder[] = ["blog", "events", "products", "popup", "email"];
+export const MEDIA_FOLDERS: MediaFolder[] = ["blog", "events", "products", "guides", "popup", "email"];
